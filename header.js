004a7f91f7700042458c0a1ce6a7452f8c0c48a5
BasovaS <basowa.www@gmail.com>
2024-04-06 15:12:34 +0300

    Style of t_form #10

--- header.js
+++
@@ -1,8 +0,0 @@
-const template = document.createElement('template');
-template.innerHTML=
-`<header>
-    <a href="index.html">Главная</a>
-    <a href="application.html">Приложение</a>
-</header>`
-
-document.body.appendChild(template.content);
