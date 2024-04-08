7e650c575a11cffc52b1e23d6ed91d70a11406ed
BasovaS <basowa.www@gmail.com>
2024-04-08 23:33:43 +0300

    #14 Создали автозаполнение формы

---
+++ t_form.js
@@ -0,0 +1,18 @@
+function Func1(){
+    document.getElementById("g_lenght").setAttribute('value', 1000);
+    document.getElementById("g_width").setAttribute('value', 200);
+    document.getElementById("g_height").setAttribute('value', 10);
+}
+
+function Func2(){
+    document.getElementById("g_lenght").setAttribute('value', 100);
+    document.getElementById("g_width").setAttribute('value', 200);
+    document.getElementById("g_height").setAttribute('value', 10);
+}
+
+function Func3(){
+    document.getElementById("g_lenght").setAttribute('value', 1000);
+    document.getElementById("g_width").setAttribute('value', 20);
+    document.getElementById("g_height").setAttribute('value', 10);
+}
+
