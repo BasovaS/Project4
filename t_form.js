34a78b673c92dd8a8186229a7d6b93b9c667c3bf
BasovaS <basowa.www@gmail.com>
2024-04-09 16:34:43 +0300

    #15 Создан полноценный функционал для кнопки "Создать"

--- t_form.js
+++ t_form.js
@@ -19,7 +19,26 @@ function Func3(){
 function CreateIm(id){
     elem = document.getElementById(id); //находим блок div по его id, который передали в функцию
     state = elem.style.display; //смотрим, включен ли сейчас элемент
-    if (state =='') elem.style.display='none'; //если включен, то выключаем
-    else elem.style.display=''; //иначе - включаем
+    
+    const formElement = document.getElementById('g_form'); // извлекаем элемент формы
+    formElement.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData(formElement); // создаём объект FormData, передаём в него элемент формы
+      // теперь можно извлечь данные
+      v_height = formData.get('g_lenght'); 
+      v_width = formData.get('g_width'); 
+      console.log(v_width);
+      document.getElementById("block").style.width =  v_width+'px';
+      document.getElementById("block").style.height =  v_height+'px';
+    });
+
+    if (state ==''){
+        elem.style.display='none'; //если включен, то выключаем
+        document.getElementById("g_create").innerHTML="Создать";
+    }
+    else {
+        elem.style.display=''; //иначе - включаем
+        document.getElementById("g_create").innerHTML="Закрыть";
+    }
 }
 
